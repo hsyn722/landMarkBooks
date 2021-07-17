395c1dfd6f3f693b3eade4531cdcd7bf01673dd3
hüseyin <hassse.1323@hotmail.com>
2021-07-17 20:32:35 +0300

    commit

--- markBooks/ViewController.swift
+++ markBooks/ViewController.swift
@@ -7,13 +7,73 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    var ericNames = [String]()
+    var ericImages = [UIImage]()
+    
+    @IBOutlet var tableView: UITableView!
+    var chosenName = ""
+    var chosenImage = UIImage()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        navigationItem.backButtonTitle = "geri"
+        navigationItem.title = "eric"
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+      
+        ericNames.append("eric one")
+        ericNames.append("eric two")
+        
+        
+        ericImages.append(UIImage(named: "eric1.jpg")!)
+        ericImages.append(UIImage(named: "eric2.png")!)
+        
+        
+    
+    }
+    
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            ericNames.remove(at: indexPath.row)
+            ericImages.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: UITableView.RowAnimation.fade)
+            
+        }
+    }
+    
+    
+    
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = ericNames[indexPath.row]
+        return cell
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return ericNames.count
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        chosenName = ericNames[indexPath.row]
+        chosenImage = ericImages[indexPath.row]
+        
+        performSegue(withIdentifier: "toImagePage", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toImagePage"{
+            var destinationVc = segue.destination as! ViewControllerImage
+            destinationVc.selectedImage = chosenImage
+            destinationVc.selectedName = chosenName
+        }
     }
 
 
-}
+    
 
+}
--- markBooks/ViewControllerImage.swift
+++ markBooks/ViewControllerImage.swift
@@ -7,23 +7,28 @@
 
 import UIKit
 
+
 class ViewControllerImage: UIViewController {
+    
+    var selectedName = ""
+    var selectedImage = UIImage()
+    
+    
+    
+    @IBOutlet var image: UIImageView!
+    @IBOutlet var label: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        label.text = selectedName
+        image.image = selectedImage
+        
 
-        // Do any additional setup after loading the view.
-    }
+       
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
-
 }
